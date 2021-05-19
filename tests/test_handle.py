cc00a173455880a0c6e54abf8c4aa12594680a9f
David Andersson <nderssonpublic@gmail.com>
2021-05-19 20:12:18 +1000

    add support for sequence in square brackets

--- tests/test_handle.py
+++ tests/test_handle.py
@@ -4,7 +4,93 @@ import pytest
 import yaml
 
 from yaml_source_map.types import Location, Entry
-from yaml_source_map.handle import primitive
+from yaml_source_map.handle import primitive, sequence
+from yaml_source_map.errors import InvalidYamlError
+
+
+SEQUENCE_TESTS = [
+    pytest.param(
+        "[]",
+        [("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 2, 2)))],
+        id="empty",
+    ),
+    pytest.param(
+        " []",
+        [("", Entry(value_start=Location(0, 1, 1), value_end=Location(0, 3, 3)))],
+        id="empty whitespace before",
+    ),
+    pytest.param(
+        "[ ]",
+        [("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 3, 3)))],
+        id="empty whitespace between",
+    ),
+    pytest.param(
+        "[] ",
+        [("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 2, 2)))],
+        id="empty whitespace after",
+    ),
+    pytest.param(
+        "[0]",
+        [
+            ("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 3, 3))),
+            ("/0", Entry(value_start=Location(0, 1, 1), value_end=Location(0, 2, 2))),
+        ],
+        id="single primitive",
+    ),
+    pytest.param(
+        "[ 0]",
+        [
+            ("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 4, 4))),
+            ("/0", Entry(value_start=Location(0, 2, 2), value_end=Location(0, 3, 3))),
+        ],
+        id="single primitive whitespace before",
+    ),
+    pytest.param(
+        "[0 ]",
+        [
+            ("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 4, 4))),
+            ("/0", Entry(value_start=Location(0, 1, 1), value_end=Location(0, 2, 2))),
+        ],
+        id="single primitive whitespace after",
+    ),
+    pytest.param(
+        "[0,0]",
+        [
+            ("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 5, 5))),
+            ("/0", Entry(value_start=Location(0, 1, 1), value_end=Location(0, 2, 2))),
+            ("/1", Entry(value_start=Location(0, 3, 3), value_end=Location(0, 4, 4))),
+        ],
+        id="multi primitive",
+    ),
+    pytest.param(
+        "[0,0,0]",
+        [
+            ("", Entry(value_start=Location(0, 0, 0), value_end=Location(0, 7, 7))),
+            ("/0", Entry(value_start=Location(0, 1, 1), value_end=Location(0, 2, 2))),
+            ("/1", Entry(value_start=Location(0, 3, 3), value_end=Location(0, 4, 4))),
+            ("/2", Entry(value_start=Location(0, 5, 5), value_end=Location(0, 6, 6))),
+        ],
+        id="many primitive",
+    ),
+]
+
+
+@pytest.mark.parametrize(
+    "source, expected_entries",
+    SEQUENCE_TESTS,
+)
+def test_sequence(source, expected_entries):
+    """
+    GIVEN source and expected entries
+    WHEN loader is created and sequence is called with the loader
+    THEN the expected entries are returned.
+    """
+    loader = yaml.Loader(source)
+    loader.get_token()
+
+    returned_entries = sequence(loader=loader)
+
+    assert returned_entries == expected_entries
 
 
 PRIMITIVE_TESTS = [
@@ -117,3 +203,23 @@ def test_primitive(source, expected_entries):
     returned_entries = primitive(loader=loader)
 
     assert returned_entries == expected_entries
+
+
+PRIMITIVE_ERROR_TESTS = [pytest.param("", id="not primitive")]
+
+
+@pytest.mark.parametrize(
+    "source",
+    PRIMITIVE_ERROR_TESTS,
+)
+def test_primitive_error(source):
+    """
+    GIVEN source
+    WHEN loader is created and primitive is called with the loader
+    THEN InvalidYamlError is raised.
+    """
+    loader = yaml.Loader(source)
+    loader.get_token()
+
+    with pytest.raises(InvalidYamlError):
+        primitive(loader=loader)
--- yaml_source_map/handle.py
+++ yaml_source_map/handle.py
@@ -1,8 +1,69 @@
 """Calculate the JSON source map for a value."""
 
+from yaml_source_map import errors
 import yaml
 
 from . import types
+
+
+def value(*, loader: yaml.Loader) -> types.TSourceMapEntries:
+    """
+    Calculate the source map of any value.
+
+    Args:
+        source: The JSON document.
+        current_location: The current location in the source.
+
+    Returns:
+        A list of JSON pointers and source map entries.
+
+    """
+    if isinstance(loader.peek_token(), yaml.FlowSequenceStartToken):
+        return sequence(loader=loader)
+    return primitive(loader=loader)
+
+
+def sequence(*, loader: yaml.Loader) -> types.TSourceMapEntries:
+    """
+    Calculate the source map of an array value.
+
+    Args:
+        loader: Source of YAML tokens.
+
+    Returns:
+        A list of JSON pointers and source map entries.
+
+    """
+    # Look for sequence start
+    token = loader.get_token()
+    if not isinstance(token, yaml.FlowSequenceStartToken):
+        raise errors.InvalidYamlError(f"expected sequence start but received {token=}")
+    value_start = types.Location(
+        token.start_mark.line, token.start_mark.column, token.start_mark.index
+    )
+
+    # Handle values
+    sequence_index = 0
+    entries: types.TSourceMapEntries = []
+    while not isinstance(loader.peek_token(), yaml.FlowSequenceEndToken):
+        value_entries = value(loader=loader)
+        entries.extend(
+            (f"/{sequence_index}{pointer}", entry) for pointer, entry in value_entries
+        )
+        sequence_index += 1
+
+        if isinstance(loader.peek_token(), yaml.FlowEntryToken):
+            loader.get_token()
+
+    # Look for sequence end
+    token = loader.get_token()
+    if not isinstance(token, yaml.FlowSequenceEndToken):
+        raise errors.InvalidYamlError(f"expected sequence end but received {token=}")
+    value_end = types.Location(
+        token.end_mark.line, token.end_mark.column, token.end_mark.index
+    )
+
+    return [("", types.Entry(value_start=value_start, value_end=value_end))] + entries
 
 
 def primitive(*, loader: yaml.Loader) -> types.TSourceMapEntries:
@@ -17,7 +78,8 @@ def primitive(*, loader: yaml.Loader) -> types.TSourceMapEntries:
 
     """
     token = loader.get_token()
-    assert isinstance(token, yaml.ScalarToken)
+    if not isinstance(token, yaml.ScalarToken):
+        raise errors.InvalidYamlError(f"expected scalar but received {token=}")
 
     return [
         (
